a8d79b9cee2b7f51c49b55d4e8660155c642cfdf
yamantips <harunyaman01@gmail.com>
2017-01-08 17:34:58 +0300

    phpsayi
    hocam yaptığım yemek projesinden ayriyeten kendi bilgilerimle kimseden
    yardım almayarak bu oyunu yaptım ek olarak

---
+++ sayitahmin.php
@@ -0,0 +1,34 @@
+﻿<?php
+session_start();
+ob_start();
+if(!$_POST){
+      $_SESSION["sayi"]=$sayi=rand(1,100); 
+      $mesaj="Bir sayı tuttuk bilin bakalım kaç!";
+}
+else{
+   $tahmin=$_POST["tahmin"];
+   $sayi=$_SESSION["sayi"];
+   if($tahmin>$sayi)
+      $mesaj="Daha küçük bir sayı giriniz.";
+   elseif($tahmin<$sayi)
+      $mesaj="Daha BÜYÜK bir sayı giriniz.";
+   else{
+      $mesaj="Tebrikler bildiniz!";
+      $_SESSION["sayi"]=$sayi=rand(1,100);
+      $mesaj.="<br><a href='index.php'>Tekrar denemek için tıklayınız.</a>";
+
+   }
+}
+?>
+<html>
+<body>
+   <form name="form1" method="post" action="index.php">
+      Tahmin: <input type="text" name="tahmin"><br>
+      <input type="submit" name="tahminet" value="Tahmin Et">
+   </form>
+
+<?php
+echo $mesaj;
+?>
+</body>
+</html>
